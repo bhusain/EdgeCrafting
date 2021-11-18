074948790b89015997c8c049be684f6cf5af26ca
bhusain <35381201+bhusain@users.noreply.github.com>
2021-11-17 18:46:33 -0800

    Added Comments

--- edgecrafting.py
+++ edgecrafting.py
@@ -100,25 +100,31 @@ count = 0
 threshold = int(.7 * 927)
 
 
+
+###################################################################
+#Load pre-processed Edge List
+###################################################################
 f = open('kidney_blob.csv', 'w')
-
-print(len(data))
-
-
 g1 = open('kidney_blob_25.txt', 'r')
 
 
+###################################################################
+#EdgeCrafting Algorithm
+###################################################################
 for x in g1:
     a_list = x.split(',')
+    #Load data for Gene A and Gene B
     i = int(a_list[0])
     j = int(a_list[1])
     #for j in range(i+1, 19215):
     if 1>0:
+        #Check if 70% of the samples have a positive expression value
         count_i = sum(m > 0 for m in data.ix[:,i])
         if count_i > threshold:
             count_j = sum(n > 0 for n in data.ix[:,j])
             if count_j > threshold:
                 N = np.c_[data.ix[:,i], data.ix[:,j]]
+                #Generate binned matrix with 0.5 resolution.
                 k = np.zeros(bins * bins)
                 for l,m in zip(N[:,0], N[:,1]):
                     if l > 0 and m > 0:
@@ -136,14 +142,16 @@ for x in g1:
                         k[ind] = k[ind] + 1
                 k = np.interp(k, (k.min(), k.max()), (0, +1))
                 test = np.reshape(k, (bins,bins))
-                #blobs_log = blob_log(test, min_sigma=1, max_sigma=10, num_sigma=10, threshold=.01)
-                #blobs_log[:, 2] = blobs_log[:, 2] * sqrt(2)
+                blobs_log = blob_log(test, min_sigma=1, max_sigma=10, num_sigma=10, threshold=.01)
+                blobs_log[:, 2] = blobs_log[:, 2] * sqrt(2)
                 blobs_dog = blob_dog(test, max_sigma=5, threshold=.25)
                 blobs_dog[:, 2] = blobs_dog[:, 2] * sqrt(2)
                 value = normalized_mutual_info_score(N[:, 0], N[:, 1])
                 #blobs_doh = blob_doh(test, max_sigma=10, threshold=.01)
-                #if len(blobs_log) > 1 or len(blobs_dog) > 1 or len(blobs_doh) > 1:
-                if len(blobs_dog) > 1 and value > 0.95:
+                # check if one or more blob detection techniquee has more than one 'blob'
+                if len(blobs_log) > 1 or len(blobs_dog) > 1 or len(blobs_doh) > 1:
+                #if len(blobs_dog) > 1 and value > 0.95:
+                    #print detected edges
                     print(i,j)
                     blobs_list = [blobs_dog]
                     colors = ['yellow', 'lime', 'red']
@@ -151,6 +159,7 @@ for x in g1:
                     sequence = zip(blobs_list, colors, titles)
                     fig, axes = plt.subplots(2, 1, figsize=(9, 8))
                     ax = axes.ravel()
+                    #Plot detected edges as a scatterplot with overlaid blobs
                     for idx, (blobs, color, title) in enumerate(sequence):
                         ax[idx].set_title(title)
                         ax[idx].imshow(test)
@@ -166,7 +175,8 @@ for x in g1:
                     plt.ylim(0, global_max)
                     plt.title(str(value))
                     plt.show()
-                    #fig.savefig('./results2/'+str(data.columns.values[i]) + '_' + str(data.columns.values[j]) + '.png')
-                    #string_val = '%s,%s,%s,%s,%0.3f\n' % (str(i), str(j), str(data.columns.values[i]), str(data.columns.values[j]), value)
-                    #f.write(string_val)
-                    #f.flush()
+                    fig.savefig('./'+str(data.columns.values[i]) + '_' + str(data.columns.values[j]) + '.png')
+                    #Write file with detecteed edges
+                    string_val = '%s,%s,%s,%s,%0.3f\n' % (str(i), str(j), str(data.columns.values[i]), str(data.columns.values[j]), value)
+                    f.write(string_val)
+                    f.flush()
